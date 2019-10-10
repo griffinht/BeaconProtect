93db402f15ca2f5b544f70f79e58d5b6cde96d5c
LemonPickles <lemonpickles1558@gmail.com>
2019-10-09 21:40:31 -0400

    made cmd package private

--- src/net/lemonpickles/BeaconProtect/Cmds/Cmd.java
+++ src/net/lemonpickles/BeaconProtect/Cmds/Cmd.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Cmd {
+class Cmd {
     protected BeaconProtect plugin;
     Cmd(BeaconProtect plugin){
         this.plugin = plugin;
